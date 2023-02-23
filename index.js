f0f7e4c33d444372c2c65b8953e1d12cbd929085
Gisele Abzalov <gisele.abzalov@gmail.com>
2023-02-23 15:37:48 -0500

    aws

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen('5002', () => console.log('listening on port 5002'))
+
+app.get('/', (req, res) => res.send('Api working 😎') )
